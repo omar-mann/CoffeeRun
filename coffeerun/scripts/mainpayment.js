6d9055c9ed11e33b8551eab4dff878c44a655cd1
omar-mann <52722867+omar-mann@users.noreply.github.com>
2021-03-14 19:49:19 -0700

    Add files via upload

---
+++ coffeerun/scripts/mainpayment.js
@@ -0,0 +1,18 @@
+(function (window) {
+    'use strict';
+    var $ = window.jQuery;
+    var PAYMENT_SELECTOR = '[data-payment="form"]';
+    var App = window.App;
+    var Payment=App.Payment;
+    var payment= new Payment(PAYMENT_SELECTOR);
+
+
+    payment.addSubmitHandler(function() {
+        var $name = $('input[name=username]').val();
+        var $title = $('input[name=title]').val();
+        
+        document.getElementById('message').innerHTML = 'Thank you for your payment, ' + $title + ' ' + $name + '.';
+        $('finished').modal();
+    });
+
+})(window);
